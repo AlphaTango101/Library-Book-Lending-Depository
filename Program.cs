1f515e86ec747aa2b9b1b877b725fa3d890181ef
AlphaTango101 <86611647+AlphaTango101@users.noreply.github.com>
2022-12-25 23:23:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookDepository
+{
+    public class Book
+    {
+        enum Status
+        {
+            OnLoan,
+            OnShelf,
+            CheckedOut,
+            InTransit
+        }
+
+        public enum Period
+        {
+            Week,
+            Month
+        }
+
+        public string ShelfPosition { get; set; }
+        public string Condition { get; set; }
+        Status BookStatus { get; set; }
+        Period LoanPeriod { get; set; }
+        public List<Reservation> Reservations { get; private set; }
+
+        public Book()
+        {
+            BookStatus = Status.InTransit;
+            LoanPeriod = Period.Week;
+        }
+
+        public void AddToReservations(Reservation newReservation)
+        {
+            Reservations.Add(newReservation);
+        }
+
+        public Period GetLoanPeriod()
+        {
+            return LoanPeriod;
+        }
+
+
+
+    }
+}
